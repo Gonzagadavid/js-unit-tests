d7fc20481f08c703e91c28b1366786f5e4c285cd
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-24 17:25:29 -0300

    Implementado requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => ({ name, feedback: () => 'Eita pessoa boa!' });
 
 module.exports = createStudent;
